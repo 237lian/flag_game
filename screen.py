72c96ee813fbe0a48ec327351f70e04cdc4e4f0a
file <lianmitbarg@gmail.com>
2023-09-18 12:56:18 +0300

    commit

--- screen.py
+++ screen.py
@@ -1,5 +1,6 @@
 import pygame
 import consts
+import random
 
 screen = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
 pygame.display.set_caption("THE FLAG")
@@ -7,4 +8,9 @@ pygame.display.set_caption("THE FLAG")
 
 def draw_game():
     screen.fill(consts.GREEN)
+    grass = pygame.transform.scale(consts.GRASS, (50, 50))
+    for i in range(20):
+        x = random.randint(0, 950)
+        y = random.randint(0, 450)
+        screen.blit(grass, (x, y))
     pygame.display.flip()
