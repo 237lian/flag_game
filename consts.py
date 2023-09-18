535e1e0f6cc69b02e55332cb480e32d7cfe96a9d
file <lianmitbarg@gmail.com>
2023-09-18 14:02:08 +0300

    commit

--- consts.py
+++ consts.py
@@ -6,3 +6,11 @@ WINDOW_HEIGHT = 500
 GREEN = (0, 128, 0)
 
 GRASS = pygame.image.load('grass.png')
+FLAG = pygame.image.load('flag.png')
+EXPLOSION = pygame.image.load('explotion.png')
+MINE = pygame.image.load('mine.png')
+SOLDIER = pygame.image.load('soldier.png')
+SNAKE = pygame.image.load('snake.png')
+SOLDIER_NIGHT = pygame.image.load('soldier_nigth.png')
+INJURY = pygame.image.load('injury.png')
+SOLDIER_2 = pygame.image.load('soldier (2).png')
