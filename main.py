940d6f7a3edf91e882b2e25e4ae7bf7812e89210
file <lianmitbarg@gmail.com>
2023-09-18 12:14:51 +0300

    commit

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import pygame
+import screen
+import consts
+
+state = {
+    "is_window_open": True
+}
 
 
 def main():
     pygame.init()
+    while state["is_window_open"]:
+        handle_user_events()
+        screen.draw_game()
+
+
+def handle_user_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            state["is_window_open"] = False
+
+
+main()
--- screen.py
+++ screen.py
@@ -2,3 +2,13 @@ import pygame
 import consts
 
 screen = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
+pygame.display.set_caption("THE FLAG")
+
+
+def draw_game():
+    screen.fill(consts.GREEN)
+    pygame.display.update()
+
+
+while True:
+    pass
